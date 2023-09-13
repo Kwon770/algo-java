f6cfd03608276355e1226bb0bcd45487182690d1
Kwon770 <sckwon770@gmail.com>
2023-09-14 01:28:50 +0900

    goormthon: day4

--- src/main/java/algo/goorm/offlineChallenge23/Day4.java
+++ src/main/java/algo/goorm/offlineChallenge23/Day4.java
@@ -1,8 +1,11 @@
-package algo;
+package algo.goorm.offlineChallenge23;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
-public class Main {
+public class Day4 {
 
     public static void main(String[] args) {
 
