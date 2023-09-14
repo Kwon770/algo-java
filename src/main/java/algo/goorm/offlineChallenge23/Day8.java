89210bc19ea6b2908f062e72c020d5771c1edcac
Kwon770 <sckwon770@gmail.com>
2023-09-14 19:50:35 +0900

    goormthon: day8

--- src/main/java/algo/goorm/offlineChallenge23/Day8.java
+++ src/main/java/algo/goorm/offlineChallenge23/Day8.java
@@ -1,9 +1,9 @@
-package algo;
+package algo.goorm.offlineChallenge23;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-
+public class Day8 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
