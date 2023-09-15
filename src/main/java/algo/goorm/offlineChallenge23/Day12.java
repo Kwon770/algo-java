7fb552d9f3be60adfea665b23a770d9517b4dc2e
Kwon770 <sckwon770@gmail.com>
2023-09-15 10:02:36 +0900

    goormthon: day12

--- src/main/java/algo/goorm/offlineChallenge23/Day12.java
+++ src/main/java/algo/goorm/offlineChallenge23/Day12.java
@@ -1,8 +1,11 @@
-package algo;
+package algo.goorm.offlineChallenge23;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class Main {
+public class Day12 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
